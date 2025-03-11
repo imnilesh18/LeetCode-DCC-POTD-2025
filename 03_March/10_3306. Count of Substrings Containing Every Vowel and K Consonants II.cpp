3635134dd5a7aeb8654a6a4dc8277c9b574f391b
imnilesh18 <nileshshrivastava7737@gmail.com>
2025-03-12 00:48:45 +0530

    3306. Count of Substrings Containing Every Vowel and K Consonants II

--- 03_March/10_3306. Count of Substrings Containing Every Vowel and K Consonants II.cpp
+++ 03_March/10_3306. Count of Substrings Containing Every Vowel and K Consonants II.cpp
@@ -1,5 +1,6 @@
 /*
  * 3306. Count of Substrings Containing Every Vowel and K Consonants II
+ * Solve - 1358. Number of Substrings Containing All Three Characters
  *
  * You are given a string word and a non-negative integer k.
  * Return the total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once and exactly k consonants.
